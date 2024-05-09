c649e68197d99be221ac47536c12cde592d21006
Mikael <50752113+MikaelVV@users.noreply.github.com>
2024-05-09 19:39:20 +0300

    MainMenu,OptionsMenu ja muuta

---
+++ Scripts/PlayerController.gd
@@ -0,0 +1,68 @@
+extends CharacterBody3D
+
+signal health_updated(health)
+signal killed()
+
+
+@export var max_health = 100
+@export var SPEED = 10
+@export var JUMP_VELOCITY = 6
+
+# Määritellään pelaajan health, pivot ja camera variablet.
+@onready var health = max_health : set = _set_health
+@onready var pivot := $Pivot
+@onready var camera := $Pivot/Camera3D
+
+# Haetaan painovoima projekti asetuksista.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+# Function "kuuntelee" kaikkia inputteja, joka tässä tapauksessa on hiiri.
+# ja määrittelee hiiren liikkuvuuden kameran kanssa.
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"): # ui_cancel tarkoittaa Esc näppäintä.
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion: # Alla määrittellään hiiren herkkyys.
+			pivot.rotate_y(-event.relative.x * 0.01)
+			camera.rotate_x(-event.relative.y * 0.01)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90)) # Tässä limitoidaan pelaajan kameran rotaatiota, että pelaaja ei voi katsoa 360 astetta ympäri.
+
+func _physics_process(delta):
+	# Määritellään pelaajan painovoima.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Määritellään pelaajan suunta inputit. Näitä voi lisäillä, kun menee Project -> Project Settings ja Input Map.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction = (pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+#Määritellään pelaajan ottama damagen määrä.
+func damage(amount):
+	_set_health(health - amount)
+
+#Nimensä mukainen funktio. Tässä toteutetaan kaikki logiikka, jolla pelaaja tuhotaan.
+func kill_player():
+	pass
+
+# Tässä funktiossa määrittellään pelaajan nykyinen ja edellinen health value. Myös toteutetaan kill_player funktio, jos pelaajan health value on 0.
+func _set_health(value):
+	var prev_health = health
+	health = clamp(value, 0, max_health)
+	if health != prev_health:
+		emit_signal("health_updated", health)
+		if health == 0:
+			kill_player()
+	
